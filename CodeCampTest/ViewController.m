363bf97b821ec11303bcb0b497396d4a2ddea304
Tuyen <vuongxuantuyen@gmail.com>
2016-08-01 09:35:26 +0700

    chang Vien to 20

--- CodeCampTest/ViewController.m
+++ CodeCampTest/ViewController.m
@@ -16,12 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CGRect screenRect=[[UIScreen mainScreen] bounds];
+    CGFloat screenWidth=screenRect.size.width;
+    CGFloat screenHeight=screenRect.size.height;
+    const CGFloat vien=20;
+    const CGFloat khe=10;
+    const CGFloat btnWidgh=(screenWidth-2*vien-8*khe)/9;
+    const CGFloat btnHeight=(screenHeight-2*vien-9*khe -vien)/11;
+    for (int i=0; i<10; i++) {
+        for (int j = 0; j<9; j++) {
+            UIButton *btn= [[UIButton alloc]initWithFrame:CGRectMake(vien+j*(khe+btnWidgh), vien+i*(khe+btnHeight), btnWidgh, btnHeight)];
+            [btn setBackgroundColor:[UIColor blueColor]];
+            [self.view addSubview:btn];
+        }
+    }
+    UIButton *btn= [[UIButton alloc]initWithFrame:CGRectMake(vien+4*(khe+btnWidgh), vien+10*(khe+btnHeight), btnWidgh, btnHeight)];
+    [btn setBackgroundColor:[UIColor orangeColor]];
+    [self.view addSubview:btn];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
